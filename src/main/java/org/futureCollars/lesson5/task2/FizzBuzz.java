d801d0d26127b929ebefbbdebd9d2f5f123df44a
T.Abramczyk1126 <t.abramczyk@gmail.com>
2024-02-08 22:10:51 +0100

    Final version - FizzBuzz

--- src/main/java/org/futureCollars/lesson5/task2/FizzBuzz.java
+++ src/main/java/org/futureCollars/lesson5/task2/FizzBuzz.java
@@ -3,21 +3,19 @@ package org.futureCollars.lesson5.task2;
 public class FizzBuzz {
     public static void main(String[] args) {
 
-        playFizzBuzz(30);
+        playFizzBuzz(31);
 
     }
 
     public static void playFizzBuzz(int number) {
-        for (int i = 1; i < number; i++) {
-            if (isDivisible(i, 3)) {
-                System.out.println("Fizz");
-            } else if (isDivisible(i, 5)) {
-                System.out.println("Buzz");
-            } else if (isDivisible(i, 3) && isDivisible(i, 5)) {
-                System.out.println("FizzBuzz");
-            } else {
-                System.out.println(i);
-            }
+        if (isDivisible(number, 3) && isDivisible(number, 5)) {
+            System.out.println("FizzBuzz");
+        } else if (isDivisible(number, 5)) {
+            System.out.println("Buzz");
+        } else if (isDivisible(number, 3)) {
+            System.out.println("Fizz");
+        } else {
+            System.out.println(number);
         }
     }
 
